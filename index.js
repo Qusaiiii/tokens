9b5b0b6a24bf9a921345b7672ffcc34a8eb02425
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2018-09-29 12:04:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
-var ServerID = "485545464242896896"; //اي دي السيرفر
-var ChannelID = "495324488406138901";// اي دي الروم
-
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = '$'
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
 
-client.on('warn', console.warn);
-
-client.on('error', console.error);
-
-
-
-client.on('ready', () => console.log('ProBot Credits Miner Discord.js Script'));
-
-client.on('disconnect', () => console.log('PROBOT credits miner had disconnected!'));
-
-client.on('reconnecting', () => console.log('PROBOT credits miner is reconnecting...'));
-
-
-function timerFunc() {
-    client.on('message', msg => {
-        client.guilds.get(ServerID).channels.get(ChannelID).send(Math.random().toString(36).substring(7))
-
-
-    });
+ client.on('message', ReeBeL => {
+if(ReeBeL.content.startsWith(prefix + 'email')) {
+function randomem() {
+let email = '';
+const ReBeL = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789._"\'';
+for (let i = 0; i < 5; i++) email += ReBeL.charAt(Math.floor(Math.random() * ReBeL.length));
+return email;
 }
-
-var timer = setTimeout(timerFunc, 1000);
-
+function randompass() {
+let pass = '';
+const CoDeS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789.!?@#$%&()-_"\'';
+for (let i = 0; i < 8; i++) pass += CoDeS.charAt(Math.floor(Math.random() * CoDeS.length));
+return pass;
+}
+const random1 = randomem();
+const random2 = randompass();
+ReeBeL.channel.send(`email : **${random1}@gmail.com**
+password : **${random2}**`);
+}});
+  
 client.login(process.env.BOT_TOKEN);
